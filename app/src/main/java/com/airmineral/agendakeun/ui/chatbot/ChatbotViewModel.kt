93b7351b05d94b3a7dd342ec8810f3610521b6c2
dngda <opowaekeno123@gmail.com>
2020-04-10 09:11:09 +0700

    Initial commit

---
+++ app/src/main/java/com/airmineral/agendakeun/ui/chatbot/ChatbotViewModel.kt
@@ -0,0 +1,13 @@
+package com.airmineral.agendakeun.ui.chatbot
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ChatbotViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/airmineral/agendakeun/ui/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.airmineral.agendakeun.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
